8d1a47e464c7c9beb2356fc7ef86df06488d2716
surhya <surhya_97@outlook.com>
2021-04-10 04:34:12 -0700

    first commit

--- src/components/TabComp.vue
+++ src/components/TabComp.vue
@@ -14,46 +14,54 @@
    
 
 
-    <div class="tab content1">Tab1 Contents</div>
+    <div class="tab content1">
+      
+    Tab1 Content
+
+    </div>
     
     <div class="tab content2">
         <div class="tab-title">
         <p>{{msg}}</p>
         </div>
+
         <div class="collapsible">
-            <section class="accordion">
-                <input type="checkbox" name="collapse" id="handle1" >
-                <table style="width:100%">
-                <tr>
-                    <td>
-                        <h3 class="handle">
-                            <label for="handle1">Blue-Sea Marine Services</label>
-                            <p>Bank of Singapore - xxxxxxxxxxxxxxxxxxx9224</p>
-                            </h3>
-                    </td>
-                    <td>
-                        <a id="angle-icon" class="ti-angle-down"></a>
-                    </td>
-                </tr> 
-                </table>
+            <section class="accordion" >
+              <article class="message" :class="accordionClasses">
+                <!-- <input type="checkbox" name="collapse" id="handle1" > -->
                 
-                <div class="content">
-                  <section class="table-section"> 
-                    <p style=" font-weight: bold;">Account Type</p>
-                    <p>Current</p>
-                     <p style=" font-weight: bold;">Bank Name</p>
-                      <p>SBM Bank (Marutius) LTD</p>
-                  </section>
-                  <section class="table-section"> 
-                    <p style=" font-weight: bold;">Name as per Bank Account</p>
-                    <p>LCV Trade Finance Fund</p>
-                     <p style=" font-weight: bold;">Account Number</p>
-                      <p>500105659541525</p>
-                  </section>
-                    
+                <div class="accordion-header" @click="toggleAccordion">
+                  <div>
+                   <label for="handle1">Blue-Sea Marine Services</label>
+                   <p>Bank of Singapore - xxxxxxxxxxxxxxxxxxx9224</p>
+                   </div>
+                    <div class="icon-header">
+                      <p><a id="angle-icon" class="ti-angle-down"></a></p>
+                   </div>
                 </div>
+              
+                <div class="accordion-outer">
+                  <div class="accordion-message">
+                  <div class="table-section">
+                      <p class="title-format" >Account Type</p>
+                      <p>Current</p>
+                      <p class="title-format" >Bank Name</p>
+                      <p>SBM Bank (Marutius) LTD</p>
+                  </div>
+                  <div class="table-section">
+                    <p class="title-format" >Name as per Bank Account</p>
+                    <p>LCV Trade Finance Fund</p>
+                    <p class="title-format">Account Number</p>
+                    <p>500105659541525</p>
+                  </div>
+                  </div>
+                     <p class="title-format" >To change your bank account details, contact <a href="">ir@siamvalidus.co.th </a> </p>
+                  </div>
+             
+                 </article>
             </section>
         </div>
+
     </div>
 
     <div class="tab content3">Tab3 Contents</div>
@@ -69,7 +77,22 @@ export default {
   name: 'TabComp',
   data () {
     return {
-      msg: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+      msg: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      isOpen: false
+    }
+  },
+  
+  methods: {
+    toggleAccordion: function() {
+      this.isOpen = !this.isOpen;
+    }
+  },
+  computed: {
+    accordionClasses: function() {
+      return {
+        'is-closed': !this.isOpen,
+      
+      };
     }
   }
 }
@@ -93,41 +116,67 @@ h3 {
    
 }
 
-.accordion > input[type="checkbox"] {
-    position: absolute;
-  
-  }
+.accordion-header{
+  max-width: 250px;
+  padding: 5px;
+  cursor: pointer;
+  display: flex;
+ 
+}
 
-  .accordion .content {
-    overflow-y: hidden;
-    height: 0;
-    transition: height 0.3s ease;
+.icon-header{
+  padding: 10px;
+}
+
+
+.accordion-outer {
+    padding: 0;
+    max-width: 485px;
+    overflow: hidden;
+    transition: 0.3s ease all;
     background-color: white;
-    
+    position: relative;
+}
+
+  .accordion-message   {
+    padding: 0;
+    max-width: 485px;
+    overflow: hidden;
+    transition: 0.3s ease all;
+    background-color: white;
+    display: flex;
+    position: relative;
   
   }
 
-  .accordion > input[type="checkbox"]:checked ~ .content {
-    height: auto;
-    overflow: visible;
-    
-   
-  }
+ .table-section{
+  
+  flex-basis: 50%;
+     
+ }
 
+ .table-float {
+   
+   float: left;
+ }
+
+  .is-closed .accordion-message {
+    max-height: 0;
+  }
+  .is-closed .accordion-outer{
+    max-height: 0;
+  }
   .accordion label {
     display: block;
   }
 
-  
+  input { display: none; }
+  input + label { display: inline-block }
 
-
-input { display: none; }
-input + label { display: inline-block }
-
-input ~ .tab { display: none }
-#tab1:checked ~ .tab.content1,
-#tab2:checked ~ .tab.content2,
-#tab3:checked ~ .tab.content3 { display: block; }
+  input ~ .tab { display: none }
+  #tab1:checked ~ .tab.content1,
+  #tab2:checked ~ .tab.content2,
+  #tab3:checked ~ .tab.content3 { display: block; }
 
 
 
@@ -172,11 +221,11 @@ input ~ .tab { display: none }
 
  }
 
- .table-section{
-   width: 135px;
-   display: inline-block;
-   
+
+ .title-format {
+   font-weight: bold;
  }
+
 
  
 </style>
